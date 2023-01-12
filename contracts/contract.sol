c3fd1be689f25fdf417fa52e673d95323eb01c3c
GNAR1ZARD <ryanamendala@gmail.com>
2023-01-11 16:59:08 -0800

    added ownership and modifier

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -2,12 +2,19 @@
 pragma solidity ^0.8.17;
 
 contract test{//wallet
-    // //address variable that is public and can have funds deposited 
-    // address payable public owner;
+    //state address variable that can send eth 
+    address payable public owner;
 
-    // constructor(){
-    //     owner=payable(msg.sender);
-    // }
+    //initalizing ownership
+    constructor(){
+        owner=payable(msg.sender);
+    }
+
+    //only be able to execute modified function if conditions are met
+    modifier ownerOnly(){
+        require(msg.sender==owner, "Not Owner");
+        _;//execute modifier
+    }
 
     //will receive any eth sent to the contract
     function recieve() external payable{}
@@ -18,13 +25,14 @@ contract test{//wallet
     }
 
     // Function to transfer Ether from this contract to address from input
-    function transfer(address payable _to, uint _amount) external{
+    function transfer(address payable _to, uint _amount) external ownerOnly{
         // Note that "to" is declared as payable
         (bool success, ) = _to.call{value: _amount}("");
         require(success, "Failed to send Ether");
     }
 }
+//npx truffle console
 //let contract=await test.deployed()
-//let accounts = await web3.eth.getAccounts()
+//let accounts=await web3.eth.getAccounts()
 //contract.recieve.sendTransaction({from: accounts[0], value: 2000000})
 //await contract.transfer(accounts[1],100000)
